27ec33a3df6f7ad1a4c2b71c4abe9f59f0be774d
Nathan Ramsey <nathanramsey@192.168.1.239>
2023-11-20 09:49:04 +0000

    test commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,7 @@ import yotoSmileIcon from '../assets/yoto-smile.png';
 import './index.css';
 
 function App() {
-  return (
-    <header className="App-header">
-      <img src={yotoSmileIcon} className="yoto-logo" alt="logo" />
-      <p>
-        Please edit, delete, manipulate, and install any required dependencies.
-      </p>
-    </header>
-  );
+  return <div>asdas</div>;
 }
 
 export default App;
